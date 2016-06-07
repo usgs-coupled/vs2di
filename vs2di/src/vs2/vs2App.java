4e3ca290bbbf1f4f1f42821cc43a784342d472db
Scott R Charlton <charlton@usgs.gov>
2016-06-07 03:10:20 +0000

    added ctor; removed static function doHeat; changed getFilePrefix; changed getFrameTitle; changed getPropertiesDirectory on windows os; updated main; changed versionCheck to be silent; added openFile for testing purposes
    git-svn-id: svn://136.177.114.72/svn_GW/vs2di/branches/vs2drti@11215 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- vs2di/src/vs2/vs2App.java
+++ vs2di/src/vs2/vs2App.java
@@ -3,6 +3,12 @@
  */
 package vs2;
 
+import java.awt.Cursor;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import mp2.*;
 import javax.swing.*;
 
@@ -16,7 +22,17 @@ public class vs2App extends mp2App implements vs2Constants {
      */
     protected static vs2App theApp;
     
-    private static boolean doHeat = false;
+    
+    /**
+     * Creates the vs2dti application
+     */
+    public vs2App() {
+        super(ClassLoader.getSystemResource("images/splash.gif"),
+                ClassLoader.getSystemResource("images/appicon.gif"),
+                false,
+                System.getProperty("user.dir"));
+    }
+    
 
     /**
      * Creates the vs2dti application
@@ -61,14 +77,6 @@ public class vs2App extends mp2App implements vs2Constants {
     }
 
     /**
-     * Gets whether or not the simulation is heat flow.
-     */
-    public static boolean doHeat() {
-        return doHeat;
-    }
-    
-
-    /**
      * Get the file filter for the document
      */
     public javax.swing.filechooser.FileFilter getDocFileFilter() {
@@ -82,28 +90,25 @@ public class vs2App extends mp2App implements vs2Constants {
      * Gets the file prefix, either "vs2dh" or "vs2dt" depending on usage
      */
     public static String getFilePrefix() {
-        if (doHeat) {
-            return "vs2dh";
-        } else {
-            return "vs2dt";
-        }
+        return "vs2drt";
     }
     
     /**
      * Gets the title for the application frame window
      */
     public String getFrameTitle() {
-        if (doHeat) {
-            return "VS2DHI Preprocessor";
-        } else {
-            return "VS2DTI Preprocessor";
-        }
+        return "VS2DRTI Preprocessor";
     }
 
     /**
      * Gets the directory containing the properties files
      */
     protected String getPropertiesDirectory() {
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            String drive = System.getenv("HOMEDRIVE");
+            String path = System.getenv("HOMEPATH");
+            return drive + path;
+        }
         return homeDirectory + System.getProperty("file.separator") + "bin";
     }
     
@@ -111,48 +116,30 @@ public class vs2App extends mp2App implements vs2Constants {
      * Gets the application properties file name
      */
     protected String getPropertiesFileName() {
-        if (doHeat) {
-            return "vs2dhi.properties";
-        } else {
-            return "vs2dti.properties";
-        }
+        return "vs2drti.properties";
     }
 
     /**
      * The main method. This is the entry point of the application.
      */
     public static void main(String[] args) {
-        String homeDir = null;
-        String splashScreenFile = "splash.gif";
-        String appIconFile = "appicon.gif";
         mp2FileChooser.useJFileChooser(true);
-        for (int i=0; i<Math.min(args.length, 3); i++) {
-            if (args[i].equalsIgnoreCase("-heat")) {
-                doHeat = true;
-                splashScreenFile = "heatsplash.gif";
-                appIconFile = "heaticon.gif";
-            } else if (args[i].equalsIgnoreCase("-w")) {
-                try {
-                    // Use windows look and feel
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                    // When launched by a windows exe program to run on jre, the
-                    // javax.swing.JFileChooser doesn't work so we use java.awt.FileDialog
-                    // instead
-                    mp2FileChooser.useJFileChooser(false);
-                } catch (Exception e) {
-                    System.out.println("Error loading L&F: " + e);
-                }
-                
-            } else {
-                homeDir = args[i];
+        try {
+            // Use windows look and feel when running under windows
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                // When launched by a windows exe program to run on jre, the
+                // javax.swing.JFileChooser doesn't work so we use java.awt.FileDialog
+                // instead
+                mp2FileChooser.useJFileChooser(false);
             }
+        } catch (Exception e) {
+            System.out.println("Error loading L&F: " + e);
         }
-        if (homeDir == null)  {
-            homeDir = System.getProperty("user.dir");
-        }
-        //theApp = new vs2App(splashScreenFile, appIconFile, homeDir);
-        theApp = new vs2App(ClassLoader.getSystemResource("images/" + splashScreenFile),
-                ClassLoader.getSystemResource("images/" + appIconFile),
+        String homeDir = System.getProperty("user.dir");;
+        theApp = new vs2App(
+                ClassLoader.getSystemResource("images/splash.gif"),
+                ClassLoader.getSystemResource("images/appicon.gif"),
                 homeDir);
     }
     
@@ -163,23 +150,24 @@ public class vs2App extends mp2App implements vs2Constants {
         if (aDoc instanceof vs2Doc) {
             return true;
         } else {
-            if (doHeat) {
-                mp2MessageBox.showMessageDialog(
-                    "This file does not contain VS2DHI data",
-                    "IO Error");
-            } else {
-                mp2MessageBox.showMessageDialog(
-                    "This file does not contain VS2DTI data",
-                    "IO Error");
-            }
+            mp2MessageBox.showMessageDialog(
+                "This file does not contain VS2DRTI data",
+                "IO Error");
             return false;
         }
     }
-
+    
     /**
      * Checks that the version is correct
      */
     protected boolean versionCheck(mp2Doc d) {
+        return versionCheck(d, false);
+    }
+    
+    /**
+     * Checks that the version is correct
+     */
+    protected boolean versionCheck(mp2Doc d, boolean quiet) {
         String serializedVersion = d.getSerializedVersion();
         String currentVersion = d.getCurrentVersion();
         if (!serializedVersion.equals(currentVersion)) {
@@ -200,13 +188,15 @@ public class vs2App extends mp2App implements vs2Constants {
             // Only allow docs that have a version number smaller then or equal
             // to the current version number
             if (serializedVersionNumber < currentVersionNumber) {
-                String [] messageLine= new String[2];
-                messageLine[0] = "Opening a Version " + serializedVersion + " document.";
-                messageLine[1] = "Do you want to convert this to a Version " + currentVersion
-                                  + " document ?";
-                int result = mp2MessageBox.showYesNoDialog(messageLine, "Warning");
-                if (result == mp2MessageBox.NO_OPTION) {
-                    return false;
+                if (!quiet) {
+                    String [] messageLine= new String[2];
+                    messageLine[0] = "Opening a Version " + serializedVersion + " document.";
+                    messageLine[1] = "Do you want to convert this to a Version " + currentVersion
+                                      + " document ?";
+                    int result = mp2MessageBox.showYesNoDialog(messageLine, "Warning");
+                    if (result == mp2MessageBox.NO_OPTION) {
+                        return false;
+                    }
                 }
             } else if (serializedVersionNumber > currentVersionNumber){
                 mp2MessageBox.showMessageDialog("Cannot open a document created by a later version of this program.",
@@ -215,22 +205,74 @@ public class vs2App extends mp2App implements vs2Constants {
             }
         }
         // Check for change in data usage
-        if (vs2App.doHeat() && 
-                ((vs2Doc) d).getUsage() == SOLUTE_TRANSPORT) {
-            String [] messageLine= new String[2];
-            messageLine[0] = "Opening a file previously used for SOLUTE transport simulation.";
-            messageLine[1] = "Do you want to use this file for ENERGY transport simulation?";
-            int result = mp2MessageBox.showYesNoDialog(messageLine, "Warning");
-            return (result == mp2MessageBox.YES_OPTION);
-        } 
-        if (!vs2App.doHeat() && 
-                ((vs2Doc) d).getUsage() == ENERGY_TRANSPORT) {
-            String [] messageLine= new String[2];
-            messageLine[0] = "Opening a file previously used for ENERGY transport simulation.";
-            messageLine[1] = "Do you want to use this for SOLUTE transport simulation?";
-            int result = mp2MessageBox.showYesNoDialog(messageLine, "Warning");
-            return (result == mp2MessageBox.YES_OPTION);
-        }
         return true;
+    }
+    
+    /**
+     * Used for testing purposes. (Modified from mp2App.openDocument()
+     */
+    public void openFile(File inFile) {
+        try {
+            // Create an input stream for object deserialization.
+            FileInputStream input = new FileInputStream(inFile);
+            ObjectInputStream objectIn = new ObjectInputStream(input);
+
+            // Create the document by deserialization from file
+            mp2Doc d = (mp2Doc) objectIn.readObject();
+
+            if (!typeCheck(d) || !versionCheck(d)) {
+                frame.setCursor(Cursor.getDefaultCursor());
+                return;
+            }
+
+            frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            currentDirectory = new String(inFile.getPath());
+            doc = d;
+            doc.setDirectory(inFile.getPath());
+            doc.setFileName(inFile.getName());
+            if (!doc.getSerializedVersion().equals(doc.getCurrentVersion())) {
+                doc.convertToCurrentVersion();
+            }
+
+            // Set the data chooser to the first item. Usually, this is the domain.
+            JComboBox dataChooser = frame.getDataChooser();
+            if (dataChooser!= null && dataChooser.getItemCount() > 0) {
+                dataChooser.setSelectedIndex(0);
+            }
+
+            // Dispose of the resources used by the previous view
+            view.dispose();
+
+            // Create a new view
+            view = createView();
+
+            // Initialize both the document and the view.
+            doc.init(this);
+            view.init(this);
+            postProcessorFrame.reset();
+
+            // Put the view in the frame, and update the frame title.
+            frame.setView(view);
+            String title = frameTitle + ": " + doc.getFileName();
+            if (doc.isChanged()) {
+                title += " *";
+            }
+            frame.setTitle(title);
+        }
+        catch(FileNotFoundException e1) {
+            mp2MessageBox.showMessageDialog(
+                "Unable to open file " + inFile.getName(),
+                "IO Error");
+        }
+        catch(IOException e2) {
+            mp2MessageBox.showMessageDialog(
+                "Unable to read from file " + inFile.getName(),
+                "IO Error");
+        }
+        catch(ClassNotFoundException  e3) {
+            mp2MessageBox.showMessageDialog(
+                "Unable to read from file " + inFile.getName(),
+                "IO Error");
+        }        
     }
 }

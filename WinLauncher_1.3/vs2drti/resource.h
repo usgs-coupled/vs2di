c45aff0f4fa964cf8b590cad0ba529deaf3b4dbf
Scott R Charlton <charlton@usgs.gov>
2016-07-06 02:12:31 +0000

    working on vs2drti launcher
    git-svn-id: svn://136.177.114.72/svn_GW/vs2di/branches/vs2drti@11424 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ WinLauncher_1.3/vs2drti/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by hti.rc
+//
+#define IDI_ICON1                       101
+#define IDS_NOJRE                       102
+#define IDS_JRE_KEY                     103
+#define IDS_CUR_VER                     104
+#define IDS_JAVAHOME                    105
+#define IDS_APPEND                      106
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

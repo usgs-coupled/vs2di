d9090315187fcd5de6eb5994aa18176895e6c8f5
Scott R Charlton <charlton@usgs.gov>
2016-07-12 02:55:41 +0000

    added vs2post launcher code and removed old vs2drti launcher code
    git-svn-id: svn://136.177.114.72/svn_GW/vs2di/branches/vs2drti@11445 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ WinLauncher_1.3/vs2post/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by phastexport.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PHASTEXPORT_DIALOG          102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_PHASTEXPORT                 107
+#define IDI_SMALL                       108
+#define IDC_PHASTEXPORT                 109
+#define IDR_MAINFRAME                   128
+#define IDS_NOJRE                       130
+#define IDS_JRE_KEY                     131
+#define IDS_CUR_VER                     132
+#define IDS_JAVAHOME                    133
+#define IDS_APPEND                      134
+#define IDS_JARFILE                     135
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
